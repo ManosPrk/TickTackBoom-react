ccf45f66ee4561d2e392a6b0b1c773c2c1377654
2paketa <manosprk@gmail.com>
2020-03-29 17:03:46 +0300

    Remove comments in playerform

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -1,16 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-// import { savePlayer } from './api/playerApi'
 import TextInput from "./common/TextInput";
-// import { toast } from "react-toastify";
 import { useCookies } from "react-cookie";
 import { toast } from "react-toastify";
-import playerStore from "../store/playerStore"
-// import TextInput from "./common/TextInput";
+import playerStore from "../store/playerStore";
 import { savePlayer } from "../actions/playerActions";
 
 function PlayerForm(props) {
     const [players, setPlayers] = useState([]);
+
 
     function handleBlur(event) {
         const { target } = event;
@@ -27,17 +25,8 @@ function PlayerForm(props) {
             toast.error('Please insert at least 4 players!');
             return;
         }
-        playersToInsert.forEach((player) => savePlayer(player));
-        /// Save to API
-        // console.log(playersToInsert);
-        // if (playersToInsert.length > 0) {
-        //     playersToInsert.forEach((player, index) => {
-        //         savePlayer(player)
-        //             .then(toast.success('player saved'))
-        //             .catch((error) => console.log(error));
-        //     });
-        // }
-        ///
+        playersToInsert.forEach((player) => savePlayer(player)
+        );
         props.history.push('/game');
     }
 
@@ -45,13 +34,6 @@ function PlayerForm(props) {
         <form onKeyPress={(event) => event.key === 'Enter' ? event.preventDefault() : null} onSubmit={handleSubmit} id="player-container">
             <h1 style={{ color: 'aliceblue' }} className="header">Set player names</h1>
             <TextInput handleBlur={handleBlur}></TextInput>
-            {/* <div className="d-flex flex-column mt-3">
-                {players.map((player, index) => {
-                    return (
-                        <input key={index} onBlur={handleBlur} id={index} type="text" className="form-control mb-3" name={player} placeholder="Name.." />
-                    );
-                })}
-            </div> */}
             <input type="submit" className="btn btn-primary" value="Start Game" />
         </form>
     );
