c42ec19e7770ce94d8ea8c2b246e27fb24a09eda
2paketa <manosprk@gmail.com>
2020-03-30 10:56:11 +0300

    moved game state to game component due to App passing props

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,38 +1,105 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Bomb from "./Bomb";
 import Dice from "./Dice";
 import Card from "./Card";
-import GameState from './GameState';
+import { getCards, getDiceSide } from "../Utilities"
 import LoserModal from './common/LoserModal';
 import { NavLink } from 'react-router-dom';
 import ResultsModal from './common/ResultsModal';
-
+import { toast } from 'react-toastify';
 
 
 function Game(props) {
-    const {
-        cards,
-        currentCard,
-        currentDiceSide,
-        handleCardClick,
-        handleDiceClick,
-        handleBombClick,
-        showLoserModal,
-        hideLoserModal,
-        players,
-        showResultsModal,
-        hideResultsModal,
-        resetGame,
-        gameOver
-    } = GameState();
+    const [cards, setCards] = useState(getCards().slice(0, 2));
+    const [currentCard, setCurrentCard] = useState('DRAW');
+    const [currentDiceSide, setCurrentDiceSide] = useState('ROLL');
+    const [roundStarted, setRoundStarted] = useState(false);
+    const [showLoserModal, setShowLoserModal] = useState(false);
+    const [showResultsModal, setShowResultsModal] = useState(false);
+    const tickAudio = new Audio('/tick.mp3');
+    const boomAudio = new Audio('/boom.mp3');
+    // const [cookies, setCookie] = useCookies([]);
+    const players = props.players;
+    const [gameOver, setGameover] = useState(false);
 
-    useEffect(() => {
-        if (!players) {
-            props.history.push('/');
-            console.log('yo');
+    if (players.length === 0) {
+        props.history.push('/');
+    }
+
+    function handleCardClick() {
+        if (currentCard !== 'DRAW') {
+            toast.error('You already drew a card')
+            return;
         }
-    }, [players, props.history]);
+        const newCardSet = cards;
+        setCurrentCard(newCardSet.shift());
+        setCards(newCardSet);
+    }
 
+    function handleDiceClick() {
+        if (currentDiceSide !== 'ROLL') {
+            toast.error('You have already rolled the dice')
+            return;
+        }
+        const randomSide = getDiceSide();
+        setCurrentDiceSide(randomSide);
+    }
+
+    function handleBombClick() {
+        if (currentDiceSide === 'ROLL'
+            && currentCard === 'DRAW') {
+            toast.error('Please draw a card and roll the dice')
+            return;
+        }
+        if (roundStarted) {
+            return;
+        }
+        startTimer(1);
+        setRoundStarted(true);
+    }
+
+    function hideLoserModal(event) {
+        event.preventDefault();
+        players.find((player) => player.id === event.target.value).roundsLost++;
+        setShowLoserModal(false);
+        if (cards.length === 0) {
+            setShowResultsModal(true);
+            setGameover(true);
+        }
+    }
+
+    function hideResultsModal(event) {
+        setShowResultsModal(false);
+        resetGame();
+    }
+
+    function resetState() {
+        setRoundStarted(false);
+        setCurrentCard('DRAW');
+        setCurrentDiceSide('ROLL')
+    }
+
+    function resetGame() {
+        setGameover(false);
+        setCards(getCards().slice(0, 2));
+        players.forEach((player) => { player.roundsLost = 0 });
+    }
+
+    function startTimer(randomExplodingTime = Math.floor(Math.random() * (40 - 10) + 10)) {
+        tickAudio.loop = true;
+
+
+        new Promise((res, rej) => {
+            tickAudio.play().then(() => {
+                setTimeout(() => {
+                    tickAudio.pause();
+                    boomAudio.play();
+                    setShowLoserModal(true);
+                    resetState();
+                }, randomExplodingTime * 1000);
+            })
+        })
+    }
 
 
     return (
