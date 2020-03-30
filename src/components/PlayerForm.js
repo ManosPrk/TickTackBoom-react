ef2369dc17d547c4969011a173a944caf4030a72
2paketa <manosprk@gmail.com>
2020-03-30 10:57:37 +0300

    refactors player logic

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -7,32 +7,14 @@ import playerStore from "../store/playerStore";
 import { savePlayer, loadPlayers } from "../actions/playerActions";
 
 function PlayerForm(props) {
-    const [players, setPlayers] = useState([]);
-
-    function handleBlur(event) {
-        const { target } = event;
-        const newPlayers = players;
-        newPlayers[target.id] = { id: null, name: target.value, roundsLost: 0 };
-        setPlayers(newPlayers)
-    }
-
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        const playersToInsert = players.filter((player) => player !== '');
-        if (playersToInsert.length < 4) {
-            toast.error('Please insert at least 4 players!');
-            return;
-        }
-        playersToInsert.forEach((player) => savePlayer(player)
-        );
+    function onSubmit(event) {
+        props.handleSubmit(event);
         props.history.push('/game');
     }
-
     return (
-        <form onKeyPress={(event) => event.key === 'Enter' ? event.preventDefault() : null} onSubmit={handleSubmit} id="player-container">
+        <form onKeyPress={(event) => event.key === 'Enter' ? event.preventDefault() : null} onSubmit={onSubmit} id="player-container">
             <h1 style={{ color: 'aliceblue' }} className="header">Set player names</h1>
-            <TextInput handleBlur={handleBlur}></TextInput>
+            <TextInput handleBlur={props.handleBlur}></TextInput>
             <input type="submit" className="btn btn-primary" value="Start Game" />
         </form>
     );
