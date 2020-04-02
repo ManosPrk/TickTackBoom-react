fd2d760bac52df0f3310e62171815668cb0bde19
2paketa <manosprk@gmail.com>
2020-04-02 13:37:13 +0300

    Adds GameInstance and Player models

---
+++ tools/socketServer/models/Player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(_id, _name, _roundsLost, _isLeader) {
+        this.id = _id;
+        this.name = _name;
+        this.roundsLost = _roundsLost;
+        this.isLeader = _isLeader;
+    }
+}
+
+module.exports = Player;
