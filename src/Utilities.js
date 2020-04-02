e913e7dd3c55ec1775cb61933814e4734686ecbc
2paketa <manosprk@gmail.com>
2020-04-02 13:37:44 +0300

    remove unnecessary comments

--- src/Utilities.js
+++ src/Utilities.js
@@ -13,32 +13,3 @@ export function getDiceSide() {
 export function getRandomSecs() {
     return Math.floor(Math.random() * (40 - 10) + 10);
 }
-
-// export function startTimer(randomExplodingTime) {
-//     let bombTickAudio = createAudioElement("./assets/audio/tick.mp3");
-//     // randomExplodingTime = 0.5;
-//     bombTickAudio.loop = true;
-//     let bombExplodingAudio = createAudioElement("./assets/audio/boom.mp3", false);
-//     let ticking = false;
-//     let exploded = false;
-
-//     return new Promise((res, rej) => {
-//         bombTickAudio.play().then(() => {
-//             ticking = true;
-//             setTimeout(() => {
-//                 bombTickAudio.pause();
-//                 bombExplodingAudio.play();
-//                 exploded = true;
-//             }, randomExplodingTime * 1000);
-//         })
-//     })
-// }
-
-// export function createAudioElement(src, loop = true) {
-//     let newSound = document.createElement('audio');
-//     console.log(src);
-//     newSound.src = src;
-//     newSound.loop = loop;
-//     newSound.play()
-//     return newSound;
-// }
