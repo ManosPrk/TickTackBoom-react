6cc11da2030c87b0d9eb58bca68928f3549953ca
2paketa <manosprk@gmail.com>
2020-04-04 21:30:50 +0300

    refactored form for reusability

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 
 function TextInput(props) {
-    const [inputs, setInputs] = useState(4);
-    const index = [];
-
-    for (let i = 0; i < inputs && i < 12; i++) {
-        index.push(i);
-    }
-
-    function handleOnClick(event) {
-        let newInputs = inputs + 1;
-        setInputs(newInputs);
-    }
+    // const data = 
+    // for (let i = 0; i <  && i < 12; i++) {
+    //     index.push(i);
+    // }
 
     return (
         <>
             <div className="d-flex flex-column mt-3">
-                {index.map((idx) => {
-                    return (
-                        <input
-                            key={idx}
-                            onBlur={props.handleBlur}
-                            id={idx}
-                            type="text"
-                            className="form-control mb-3"
-                            // name={props.players}
-                            placeholder="Name.."
-                        />
-                    );
-                })}
+                {
+                    Object.keys(props.data).map((item, index) => {
+                        return (
+                            <input
+                                onBlur={props.handleBlur}
+                                id={index}
+                                key={index}
+                                type="text"
+                                className="form-control mb-3"
+                                name={item}
+                                placeholder="Name.."
+                            />
+                        );
+                    })
+                }
             </div>
-            <button className="btn btn-primary float-right" onClick={handleOnClick}>+</button>
         </>
     );
+}
+
+TextInput.propTypes = {
+    data: PropTypes.object.isRequired,
+    handleBlur: PropTypes.func
 }
 
 export default TextInput;
