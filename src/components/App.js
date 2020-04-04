9ec4229bc55c40ef7f8e5b96f45d2326e084a337
2paketa <manosprk@gmail.com>
2020-04-04 21:30:08 +0300

    refactored app logic

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,17 @@ import Game from './Game';
 import { ToastContainer } from 'react-toastify';
 import PlayerForm from './PlayerForm';
 import TextInput from './common/TextInput';
+import JoinGameForm from './JoinGameForm';
 
 function App(props) {
-
-
     return (
         <div className="container-fluid">
             <ToastContainer newestOnTop autoClose={2000} hideProgressBar />
             <Switch>
                 <Route path="/" exact component={MainMenu} />
-                <Route
-                    path="/players" exact component={PlayerForm}
-                />
-                <Route
-                    path="/game/:id" exact component={Game}
-                />
+                <Route path="/players" exact component={PlayerForm} />
+                <Route path="/join" exact component={JoinGameForm} />
+                <Route path="/game/:id" exact component={Game} />
                 <Redirect from="/game" to="/" />
                 <Route path="/inputs" exact component={TextInput} />
             </Switch>
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,20 +1,54 @@
 import React from "react";
 import Bomb from "./Bomb";
 import { NavLink } from "react-router-dom";
+import ModalTemplate from "./common/ModalTemplate"
+import FormTemplate from "./common/FormTemplate";
+import { toast } from "react-toastify";
+import { createGameInstance } from "../socket_helper/playerSocket";
+import { useState } from "react";
+import ModalFormTemplate from "./common/ModalFormTemplate";
 
-function MainMenu() {
+function MainMenu(props) {
+    const [formData, setFormData] = useState({
+        name: ''
+    });
+
+    function handleBlur(event) {
+        event.preventDefault();
+        const { target } = event;
+        formData[event.target.name] = target.value;
+    }
+
+    function handleNewGameSubmit(event) {
+        event.preventDefault();
+        createGameInstance(formData, (ioResponse) => {
+            console.log(ioResponse);
+            props.history.push(`/game/${ioResponse.gameId}`);
+        })
+    }
 
     return (
         <div id="main-menu-container" className="d-flex flex-column justify-content-evenly align-items-center">
             <Bomb></Bomb>
             <div>
                 <nav className="d-flex flex-column col-md-12">
+                    <ModalFormTemplate
+                        modalTitle='Enter your name to start a new game!'
+                        modalButtonText="New Game"
+                        formMutedText="we care"
+                        formButtonText="Submit"
+                        formHandleSubmit={handleNewGameSubmit}
+                        formLabel="Name"
+                        formInputPlaceholder="Player..."
+                        formInputHandleBlur={handleBlur}
+                        formInputName={Object.keys(formData)}
+                    />
                     <h1>
-                        <NavLink className="nav-button" id="nav-button" to="/players">New Game</NavLink>
+                        <NavLink className="nav-button" id="nav-button" to="/join">Join Game</NavLink>
                     </h1>
                 </nav>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
