0ce41d44361ec515470eaeaa3a5a140919b7a1e7
2paketa <manosprk@gmail.com>
2020-04-11 15:02:24 +0300

    changes to server domain

--- src/socket_helper/playerSocket.js
+++ src/socket_helper/playerSocket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const socket = io.connect('192.168.1.8:1337');
+const socket = io.connect('localhost:1337');
 
 export function getSocketId() {
     return socket.id;
